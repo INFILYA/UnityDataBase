ca6d38a43ba87f1c8e0aede45aa5b2812a5be56e
Philip Harmash <infilya@ukr.net>
2024-03-20 09:38:21 -0400

    back

--- src/HMF/components/Auth.tsx
+++ src/HMF/components/Auth.tsx
@@ -58,7 +58,7 @@ export function Auth() {
       setIsLoginLoading(true);
       // ПОмеНЯТЬ ЛиНКДОМАИН !!!
       await sendSignInLinkToEmail(auth, email, {
-        url: "unity-data-base-628a9.firebaseapp.com",
+        url: "https://unity-data-base-628a9.web.app",
         handleCodeInApp: true,
         dynamicLinkDomain: "unitydatabase.page.link",
       });
