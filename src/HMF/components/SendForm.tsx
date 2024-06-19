21cd87e41121447b5945f3d998b285006aed67e7
Philip Harmash <infilya@ukr.net>
2024-06-19 14:07:33 -0400

    now ok

--- src/HMF/components/SendForm.tsx
+++ src/HMF/components/SendForm.tsx
@@ -83,7 +83,6 @@ export default function SendForm() {
         ...userInfo,
         position: "",
         team: "",
-        previousTeam: "",
         gender: "",
         hand: "",
         height: "",
@@ -156,7 +155,6 @@ export default function SendForm() {
 
   const properPhoneLength = userInfo.telephone.length !== 12;
   const disabledButton = isEmptyFields || properPhoneLength || checkPhotoFormat(userInfo.photo);
-  console.log(userInfo);
   return (
     <SectionWrapper>
       <FormWrapper onSubmit={submitUserInfo}>
